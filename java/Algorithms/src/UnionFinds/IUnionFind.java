1b96b5cea8fa9003ec14f3dc3730468e98dce31f
BigEgg <cd_bigegg@hotmail.com>
2014-10-06 00:28:27 +0800

    Add the comments for the methods; Add find and count methods.

--- java/Algorithms/src/UnionFinds/IUnionFind.java
+++ java/Algorithms/src/UnionFinds/IUnionFind.java
@@ -4,7 +4,40 @@ package UnionFinds;
  * Created by jianming.xiao on 10/5/14.
  */
 public interface IUnionFind {
-    void union(int p, int q);
+    /**
+     * Merges the component containing site<tt>p</tt> with the component
+     * containing site <tt>q</tt>.
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
+    void union(int p, int q) throws IndexOutOfBoundsException;
 
-    boolean connected(int p, int q);
+    /**
+     * Are the two sites <tt>p</tt> and <tt>q</tt> in the same component?
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @return <tt>true</tt> if the two sites <tt>p</tt> and <tt>q</tt>
+     * are in the same component, and <tt>false</tt> otherwise
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
+    boolean connected(int p, int q) throws IndexOutOfBoundsException;
+
+    /**
+     * Returns the number of components.
+     *
+     * @return the number of components (between 1 and N)
+     */
+    int count();
+
+    /**
+     * Returns the component identifier for the component containing site <tt>p</tt>.
+     *
+     * @param p the integer representing one site
+     * @return the component identifier for the component containing site <tt>p</tt>
+     * @throws java.lang.IndexOutOfBoundsException unless 0 <= p < N
+     */
+    int find(int p) throws IndexOutOfBoundsException;
 }
--- java/Algorithms/src/UnionFinds/WeightedQuickUnionUF.java
+++ java/Algorithms/src/UnionFinds/WeightedQuickUnionUF.java
@@ -4,8 +4,14 @@ package UnionFinds;
  * Created by jianming.xiao on 10/5/14.
  */
 public class WeightedQuickUnionUF extends QuickUnionUF {
-    private int[] sz;
+    private int[] sz;           // sz[i] = number of objects in subtree rooted at i
 
+    /**
+     * Initializes an empty union-find data structure with N isolated components 0 through N-1.
+     *
+     * @param N the number of objects
+     * @throws java.lang.IllegalArgumentException if N < 0
+     */
     public WeightedQuickUnionUF(int N) {
         super(N);
         sz = new int[N];
@@ -14,18 +20,30 @@ public class WeightedQuickUnionUF extends QuickUnionUF {
         }
     }
 
+    /**
+     * Merges the component containing site<tt>p</tt> with the component
+     * containing site <tt>q</tt>.
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
     @Override
-    public void union(int p, int q) {
-        int i = root(p);
-        int j = root(q);
-        if (i == j) return;
+    public void union(int p, int q) throws IndexOutOfBoundsException{
+        if (p < 0 || p >= id.length) throw new IndexOutOfBoundsException();
+        if (q < 0 || q >= id.length) throw new IndexOutOfBoundsException();
 
-        if (sz[i] < sz[j]) {
-            id[i] = j;
-            sz[j] += sz[i];
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) return;
+
+        if (sz[pRoot] < sz[qRoot]) {
+            id[pRoot] = qRoot;
+            sz[qRoot] += sz[pRoot];
         } else {
-            id[j] = i;
-            sz[i] += sz[j];
+            id[qRoot] = pRoot;
+            sz[pRoot] += sz[qRoot];
         }
+        count--;
     }
 }
