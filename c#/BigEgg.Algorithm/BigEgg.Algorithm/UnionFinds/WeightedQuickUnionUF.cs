498beedaa42ec9c37daabe28d8bb45e0e813c9c4
BigEgg <cd_bigegg@hotmail.com>
2014-10-05 19:26:23 +0800

    Add algorithm Weighted-Quick-Union for problem: Union-Find.

---
+++ c#/BigEgg.Algorithm/BigEgg.Algorithm/UnionFinds/WeightedQuickUnionUF.cs
@@ -0,0 +1,37 @@
+﻿
+namespace BigEgg.Algorithm.UnionFinds
+{
+    public class WeightedQuickUnionUF : QuickUnionUF
+    {
+        private int[] sz;
+
+        public WeightedQuickUnionUF(int N)
+            : base(N)
+        {
+            sz = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                sz[i] = 1;
+            }
+        }
+
+        public override void Union(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            if (i == j) return;
+
+            if (sz[i] < sz[j])
+            {
+                id[i] = j;
+                sz[j] += sz[i];
+            }
+            else
+            {
+                id[j] = i;
+                sz[i] += sz[j];
+            }
+        }
+
+    }
+}
