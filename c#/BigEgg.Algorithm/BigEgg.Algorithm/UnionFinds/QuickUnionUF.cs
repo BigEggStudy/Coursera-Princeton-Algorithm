ac0c59a2468a47699c30fe2bed4380c7dd059173
BigEgg <cd_bigegg@hotmail.com>
2014-10-05 18:52:38 +0800

    Add algorithm Quick-Union for problem: Union-Find.

---
+++ c#/BigEgg.Algorithm/BigEgg.Algorithm/UnionFinds/QuickUnionUF.cs
@@ -0,0 +1,27 @@
+﻿namespace BigEgg.Algorithm.UnionFinds
+{
+    public class QuickUnionUF : UnionFind
+    {
+        public QuickUnionUF(int N)
+            : base(N)
+        { }
+
+        public override void Union(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            id[i] = j;
+        }
+
+        public override bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        private int Root(int i)
+        {
+            while (i != id[i]) i = id[i];
+            return i;
+        }
+    }
+}
