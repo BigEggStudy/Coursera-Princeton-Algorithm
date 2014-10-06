64d5e6470186be75b5fcc6d700704894f4cb7e0d
BigEgg <cd_bigegg@hotmail.com>
2014-10-06 17:49:58 +0800

    Add unit test for Binary Search. Fix bugs of it.

--- c#/BigEgg.Algorithm/BigEgg.Algorithm/BinarySearch.cs
+++ c#/BigEgg.Algorithm/BigEgg.Algorithm/BinarySearch.cs
@@ -3,17 +3,12 @@
     public static class BinarySearch
     {
         /// <summary>
-        /// Prevents a default instance of the <see cref="BinarySearch"/> class from being created.
-        /// </summary>
-        private BinarySearch() { }
-
-        /// <summary>
         /// Searches for the integer key in the sorted array a[].
         /// </summary>
         /// <param name="a">The array of integers, must be sorted in ascending order.</param>
         /// <param name="key">The search key.</param>
         /// <returns>Index of key in array a[] if present; -1 if not present.</returns>
-        public int Rank(int[] a, int key)
+        public static int Rank(int[] a, int key)
         {
             int lo = 0;
             int hi = a.Length - 1;
