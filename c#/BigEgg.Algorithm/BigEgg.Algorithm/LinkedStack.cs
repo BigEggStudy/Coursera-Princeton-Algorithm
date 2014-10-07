457ed86b0140fbb6f3aa16c72666cee12220d301
BigEgg <cd_bigegg@hotmail.com>
2014-10-07 17:12:29 +0800

    Add the stack interface and the Linked-Stack implementation.

---
+++ c#/BigEgg.Algorithm/BigEgg.Algorithm/LinkedStack.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigEgg.Algorithm
+{
+    public class LinkedStack<Item> : IStack<Item>
+    {
+        private int N;
+        private Node first;
+
+        private class Node
+        {
+            public Item Item { get; set; }
+            public Node Next { get; set; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedStack{Item}"/> class.
+        /// </summary>
+        public LinkedStack()
+        {
+            N = 0;
+            first = null;
+        }
+
+        /// <summary>
+        /// Is this stack empty?
+        /// </summary>
+        /// <returns><c>True</c> if this stack is empty; <c>false</c> otherwise</returns>
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+
+        /// <summary>
+        /// Returns the number of items in the stack.
+        /// </summary>
+        /// <returns>The number of items in the stack.</returns>
+        public int Size()
+        {
+            return N;
+        }
+
+        /// <summary>
+        /// Adds the item to this stack.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
+        /// <exception cref="System.ArgumentNullException">Item cannot be null.</exception>
+        public void Push(Item item)
+        {
+            if (item == null) { throw new ArgumentNullException("item"); }
+            var oldFirst = first;
+            first = new Node();
+            first.Item = item;
+            first.Next = oldFirst;
+            N++;
+        }
+
+        /// <summary>
+        /// Removes and returns the item most recently added to this stack.
+        /// </summary>
+        /// <returns>The item most recently added.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">Stack is empty.</exception>
+        public Item Pop()
+        {
+            if (IsEmpty()) { throw new ArgumentOutOfRangeException(); }
+            var result = first.Item;
+            first = first.Next;
+            N--;
+            return result;
+        }
+
+        /// <summary>
+        /// Returns (but does not remove) the item most recently added to this stack.
+        /// </summary>
+        /// <returns>The item most recently added to this stack.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">Stack is empty.</exception>
+        public Item Peek()
+        {
+            if (IsEmpty()) { throw new ArgumentOutOfRangeException(); }
+            return first.Item;
+        }
+
+        /// <summary>
+        /// Returns a string representation of this stack.
+        /// </summary>
+        /// <returns>The sequence of items in the stack in LIFO order, separated by spaces.</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this)
+            {
+                sb.Append(item.ToString()).Append(" ");
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the items in LIFO order.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the items in LIFO order.
+        /// </returns>
+        IEnumerator<Item> IEnumerable<Item>.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a items in LIFO order.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the items in LIFO order.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        private struct Enumerator : IEnumerator<Item>, IDisposable, IEnumerator
+        {
+            private Node current;
+            private Node first;
+
+            public Enumerator(LinkedStack<Item> stack)
+            {
+                current = stack.first;
+                first = stack.first;
+            }
+
+
+            public Item Current
+            {
+                get { return current.Item; }
+            }
+
+            public void Dispose()
+            {
+                first = current = null;
+            }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get { return current.Item; }
+            }
+
+            public bool MoveNext()
+            {
+                current = current.Next;
+                return current != null;
+            }
+
+            public void Reset()
+            {
+                current = first;
+            }
+        }
+    }
+}
