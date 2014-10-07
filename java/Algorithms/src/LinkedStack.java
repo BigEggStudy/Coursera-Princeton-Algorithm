1472eb0dd63beaae3f99bed57fcbd0aab78bb8b8
BigEgg <cd_bigegg@hotmail.com>
2014-10-07 15:17:46 +0800

    Add LinkedStack data type.

---
+++ java/Algorithms/src/LinkedStack.java
@@ -0,0 +1,134 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by jianming.xiao on 10/7/14.
+ */
+public class LinkedStack<Item> implements IStack<Item> {
+    private int N;
+    private Node first;
+
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+
+    /**
+     * Initializes an empty stack.
+     */
+    public LinkedStack() {
+        first = null;
+        N = 0;
+    }
+
+    /**
+     * Is this stack empty?
+     *
+     * @return true if this stack is empty; false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Returns the number of items in the stack.
+     *
+     * @return the number of items in the stack
+     */
+    @Override
+    public int size() {
+        return N;
+    }
+
+    /**
+     * Adds the item to this stack.
+     *
+     * @param item the item to add
+     * @throws NullPointerException if the <tt>item</tt> is null;
+     */
+    @Override
+    public void push(Item item) {
+        if (item == null) throw new NullPointerException();
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    /**
+     * Removes and returns the item most recently added to this stack.
+     *
+     * @return the item most recently added
+     * @throws java.util.NoSuchElementException if this stack is empty
+     */
+    @Override
+    public Item pop() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        Item result = first.item;
+        first = first.next;
+        N--;
+        return result;
+    }
+
+    /**
+     * Returns (but does not remove) the item most recently added to this stack.
+     *
+     * @return the item most recently added to this stack
+     * @throws java.util.NoSuchElementException if this stack is empty
+     */
+    @Override
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        return first.item;
+    }
+
+    /**
+     * Returns a string representation of this stack.
+     *
+     * @return the sequence of items in the stack in LIFO order, separated by spaces
+     */
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder =new StringBuilder();
+        for (Item item : this) {
+            stringBuilder.append(item.toString()).append(" ");
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Returns an iterator to this stack that iterates through the items in LIFO order.
+     *
+     * @return an iterator to this stack that iterates through the items in LIFO order.
+     */
+    @Override
+    public Iterator<Item> iterator() {
+        return new LinkedStackIterator();
+    }
+
+    private class LinkedStackIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item result = current.item;
+            current = current.next;
+            return result;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
