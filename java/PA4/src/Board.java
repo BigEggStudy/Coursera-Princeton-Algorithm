7e6c9932faeaa5edf7fda47e3bf53ea697c0f933
BigEgg <cd_bigegg@hotmail.com>
2014-10-12 20:22:30 +0800

    Add the APIs.

---
+++ java/PA4/src/Board.java
@@ -0,0 +1,51 @@
+/**
+ * Created by jianming.xiao on 10/12/14.
+ */
+public class Board {
+    // construct a board from an N-by-N array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+
+    }
+
+    // board dimension N
+    public int dimension() {
+
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+
+    }
+
+    // a boadr that is obtained by exchanging two adjacent blocks in the same row
+    public Board twin() {
+
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+    }
+
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+    }
+
+    // unit tests (not graded)
+    public static void main(String[] args) {
+    }
+}
