4eaad90ce7cc0dce2188582fe0d9693e1816651a
BigEgg <cd_bigegg@hotmail.com>
2014-10-12 23:25:59 +0800

    Complete the code of Solver. Not check the unsolved situation.

--- java/PA4/src/Solver.java
+++ java/PA4/src/Solver.java
@@ -2,20 +2,82 @@
  * Created by jianming.xiao on 10/12/14.
  */
 public class Solver {
+    private MinPQ<SearchNode> pq;
+    private MinPQ<SearchNode> results;
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private Board board;
+        private int moves;
+        private SearchNode previousNode;
+
+        private int priority() {
+            return this.board.manhattan() + this.moves;
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            if ((this.priority()) > (that.priority())) return 1;
+            if ((this.priority()) < (that.priority())) return -1;
+            if ((this.board.manhattan()) > (that.board.manhattan())) return 1;
+            if ((this.board.manhattan()) < (that.board.manhattan())) return -1;
+            if ((this.board.hamming()) > (that.board.hamming())) return 1;
+            if ((this.board.hamming()) < (that.board.hamming())) return -1;
+            return 0;
+        }
+    }
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        pq = new MinPQ<SearchNode>();
+        results = new MinPQ<SearchNode>();
+
+        SearchNode initialNode = new SearchNode();
+        initialNode.board = initial;
+        initialNode.moves = 0;
+        pq.insert(initialNode);
+
+        while (pq.isEmpty()) {
+            SearchNode node = pq.delMin();
+            if (node.board.isGoal()) {
+                results.insert(node);
+                continue;
+            }
+            for (Board neighbors : node.board.neighbors()) {
+                if (node.previousNode != null && neighbors == node.previousNode.board) {
+                    continue;
+                }
+                SearchNode newNode = new SearchNode();
+                newNode.board = neighbors;
+                newNode.moves = node.moves + 1;
+                newNode.previousNode = node;
+                pq.insert(newNode);
+            }
+        }
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
+        return !results.isEmpty();
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
+        if (results.isEmpty()) {
+            return -1;
+        } else {
+            return results.min().moves;
+        }
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
+        Stack<Board> result = new Stack<Board>();
+        SearchNode minMove = results.delMin();
+        while (minMove != null) {
+            result.push(minMove.board);
+            minMove = minMove.previousNode;
+        }
+        return result;
     }
 
     // solve a slider puzzle
