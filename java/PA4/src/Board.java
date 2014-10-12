91bb2ef6922286c34716a54ed6eeb6eda4f1e93d
BigEgg <cd_bigegg@hotmail.com>
2014-10-12 22:58:48 +0800

    Save the hamming and manhattan value.

--- java/PA4/src/Board.java
+++ java/PA4/src/Board.java
@@ -6,20 +6,46 @@ import java.util.Arrays;
 public class Board {
     private int N;
     private char[] a;
-    private int blankIndex;
+    private int blankRow;
+    private int blankCol;
+    private int manhattanSum;
+    private int hammingSum;
 
     // construct a board from an N-by-N array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
         N = blocks.length;
         a = new char[N * N];
+        manhattanSum = 0;
+        hammingSum = 0;
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                a[i * N + j] = (char) blocks[i][j];
-                if (blocks[i][j] == 0) blankIndex = i * N + j;
+                int index = i * N + j;
+                a[index] = (char) blocks[i][j];
+
+                if (blocks[i][j] == 0) {
+                    blankRow = i;
+                    blankCol = j;
+                    continue;
+                }
+
+                int goalRow = blocks[i][j] / N;
+                int goalCol = blocks[i][j] % N;
+                manhattanSum += Math.abs(j - goalCol) + Math.abs(i - goalRow);
+
+                if (blocks[i][j] != index + 1) hammingSum++;
             }
         }
+    }
+
+    private Board(char[] a, int N, int hammingSub, int manhattanSum, int blankRow, int blankCol) {
+        this.N = N;
+        this.a = a;
+        this.blankRow = blankRow;
+        this.blankCol = blankCol;
+        this.hammingSum = hammingSub;
+        this.manhattanSum = manhattanSum;
     }
 
     // board dimension N
@@ -29,25 +55,12 @@ public class Board {
 
     // number of blocks out of place
     public int hamming() {
-        int count = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != i + 1) count++;
-        }
-        return count;
+        return hammingSum;
     }
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        int count = 0;
-        for (int i = 0; i < a.length; i++) {
-            int currentRow = i / N;
-            int currentCol = i % N;
-            int goalRow = a[i] / N;
-            int goalCol = a[i] % N;
-
-            count += Math.abs(currentCol - goalCol) + Math.abs(currentRow - goalRow);
-        }
-        return count;
+        return manhattanSum;
     }
 
     // is this board the goal board?
@@ -63,13 +76,30 @@ public class Board {
 
     // a boadr that is obtained by exchanging two adjacent blocks in the same row
     public Board twin() {
-        int blankRow = blankIndex / N;
         int exchRow = -1;
         while (++exchRow < N) {
             if (exchRow != blankRow) break;
         }
 
-        return exch(exchRow, 0, exchRow, 1);
+        int index1 = exchRow * N;
+        char block1 = a[index1];
+        int goalRow1 = block1 / N;
+        int goalCol1 = block1 % N;
+        int deltaManhattan1 = Math.abs(0 - goalCol1) + Math.abs(exchRow - goalRow1) -
+                Math.abs(1 - goalCol1) + Math.abs(exchRow - goalRow1);
+        int deltaHamming1 = block1 != index1 + 1 ? 1 : 0;
+        deltaHamming1 -= block1 != index1 + 2 ? 1 : 0;
+
+        int index2 = exchRow * N + 1;
+        char block2 = a[index2];
+        int goalRow2 = block2 / N;
+        int goalCol2 = block2 % N;
+        int deltaManhattan2 = Math.abs(1 - goalCol2) + Math.abs(exchRow - goalRow2) -
+                Math.abs(0 - goalCol2) + Math.abs(exchRow - goalRow2);
+        int deltaHamming2 = block1 != index1 + 1 ? 1 : 0;
+        deltaHamming2 -= block1 != index1 ? 1 : 0;
+
+        return new Board(exch(exchRow, 0, exchRow, 1), N, hammingSum - deltaHamming1 - deltaHamming2, manhattanSum - deltaManhattan1 - deltaManhattan2, blankRow, blankCol);
     }
 
     // does this board equal y?
@@ -83,36 +113,44 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        int blankRow = blankIndex / N;
-        int blankCol = blankIndex % N;
         Stack<Board> stack = new Stack<Board>();
         if (blankCol > 0) {
-            stack.push(exch(blankRow, blankCol - 1, blankRow, blankCol));
+            stack.push(getNeighbor(0, -1));
         }
         if (blankCol < N - 1) {
-            stack.push(exch(blankRow, blankCol + 1, blankRow, blankCol));
+            stack.push(getNeighbor(0, 1));
         }
         if (blankRow > 0) {
-            stack.push(exch(blankRow - 1, blankCol, blankRow, blankCol));
+            stack.push(getNeighbor(-1, 0));
         }
         if (blankRow < N - 1) {
-            stack.push(exch(blankRow + 1, blankCol, blankRow, blankCol));
+            stack.push(getNeighbor(1, 0));
         }
         return stack;
     }
 
-    private Board exch(int row1, int col1, int row2, int col2) {
-        int[][] temp = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                temp[i][j] = a[i * N + j];
-            }
+    private Board getNeighbor(int deltaRow, int deltaCol) {
+        int index = (blankRow + deltaRow) * N + (blankCol + deltaCol);
+        char block = a[index];
+        int goalRow = block / N;
+        int goalCol = block % N;
+        int deltaManhattan = Math.abs(blankCol + deltaCol - goalCol) + Math.abs(blankRow + deltaRow - goalRow) -
+                Math.abs(blankCol - goalCol) + Math.abs(blankRow - goalRow);
+        int deltaHamming = block != index + 1 ? 1 : 0;
+        deltaHamming -= block != index + 1 - deltaCol + N * deltaRow ? 1 : 0;
+        return new Board(exch(blankRow + deltaRow, blankCol + deltaCol, blankRow, blankCol), N, hammingSum - deltaHamming, manhattanSum - deltaManhattan, blankRow, blankCol - 1);
+    }
+
+    private char[] exch(int row1, int col1, int row2, int col2) {
+        char[] temp = new char[a.length];
+        for (int i = 0; i < a.length; i++) {
+            temp[i] = a[i];
         }
 
-        int exch = temp[row1][col1];
-        temp[row1][col1] = temp[row2][col2];
-        temp[row2][col2] = exch;
-        return new Board(temp);
+        char exch = temp[row1 * N + col1];
+        temp[row1 * N + col1] = temp[row2 * N + col2];
+        temp[row2 * N + col2] = exch;
+        return temp;
     }
 
     // string representation of this board (in the output format specified below)
