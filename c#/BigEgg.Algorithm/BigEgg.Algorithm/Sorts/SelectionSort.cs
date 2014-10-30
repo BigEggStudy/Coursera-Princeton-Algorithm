c273de5185c0b0bfce7d976f9f7429bfbeca0885
BigEgg <cd_bigegg@hotmail.com>
2014-10-30 23:37:45 +0800

    Add the selection sort algorithm

---
+++ c#/BigEgg.Algorithm/BigEgg.Algorithm/Sorts/SelectionSort.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+
+namespace BigEgg.Algorithm.Sorts
+{
+    public static class SelectionSort
+    {
+        /// <summary>
+        /// Rearranges the array in ascending order, using the natural order.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        public void Sort(IComparable[] a)
+        {
+            Sort(a, SortOrder.ASC);
+        }
+
+        /// <summary>
+        /// Rearranges the array in ascending order, using a comparer.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        /// <param name="c">the comparator that specifying the order</param>
+        public void Sort(Object[] a, IComparer c)
+        {
+            Sort(a, c, SortOrder.ASC);
+        }
+
+        /// <summary>
+        /// Rearranges the array in specific order, using the natural order.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        /// <param name="order">The sort order.</param>
+        public void Sort(IComparable[] a, SortOrder order)
+        {
+            if (a == null) { throw new ArgumentNullException("a"); }
+
+            if (order == SortOrder.ASC)
+                SortASC(a);
+            else
+                SortDESC(a);
+        }
+
+        /// <summary>
+        /// Rearranges the array in specific order, using a comparer.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        /// <param name="c">the comparator that specifying the order</param>
+        /// <param name="order">The sort order.</param>
+        public void Sort(Object[] a, IComparer c, SortOrder order)
+        {
+            if (a == null) { throw new ArgumentNullException("a"); }
+            if (c == null) { throw new ArgumentNullException("c"); }
+
+            if (order == SortOrder.ASC)
+                SortASC(a, c);
+            else
+                SortDESC(a, c);
+        }
+
+        /// <summary>
+        /// Rearranges the array in ascending order, using the natural order.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        protected void SortASC(IComparable[] a)
+        {
+            int N = a.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < N; j++)
+                    if (SortHelper.Less(a[j], a[min]))
+                        min = j;
+                SortHelper.Exchange(a, min, i);
+            }
+        }
+
+        /// <summary>
+        /// Rearranges the array in ascending order, using a comparer.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        /// <param name="c">the comparator that specifying the order</param>
+        protected void SortASC(Object[] a, IComparer c)
+        {
+            int N = a.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < N; j++)
+                    if (SortHelper.Less(c, a[j], a[min]))
+                        min = j;
+                SortHelper.Exchange(a, i, min);
+            }
+        }
+
+        /// <summary>
+        /// Rearranges the array in descending order, using the natural order.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        protected void SortDESC(IComparable[] a)
+        {
+            int N = a.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                int max = i;
+                for (int j = i + 1; j < N; j++)
+                    if (SortHelper.Greater(a[j], a[max]))
+                        max = j;
+                SortHelper.Exchange(a, max, i);
+            }
+        }
+
+        /// <summary>
+        /// Rearranges the array in descending order, using a comparer.
+        /// </summary>
+        /// <param name="a">The array to be sorted</param>
+        /// <param name="c">the comparator that specifying the order</param>
+        protected void SortDESC(Object[] a, IComparer c)
+        {
+            int N = a.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                int max = i;
+                for (int j = i + 1; j < N; j++)
+                    if (SortHelper.Greater(c, a[j], a[max]))
+                        max = j;
+                SortHelper.Exchange(a, i, max);
+            }
+        }
+    }
+}
