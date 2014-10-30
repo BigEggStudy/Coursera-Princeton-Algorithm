faf30d50be0390faf2b83a9407088a28a02e69dd
BigEgg <cd_bigegg@hotmail.com>
2014-10-30 15:31:06 +0800

    Add sort logic for object and comparator

--- java/Algorithms/src/Sorts/SelectionSort.java
+++ java/Algorithms/src/Sorts/SelectionSort.java
@@ -1,5 +1,7 @@
 package Sorts;
 
+import java.util.Comparator;
+
 import static Sorts.SortHelper.*;
 
 /**
@@ -37,6 +39,29 @@ public class SelectionSort {
     }
 
     /**
+     * Rearranges the array in ascending order, using a comparator.
+     *
+     * @param a the array
+     * @param c the comparator specifying the order
+     */
+    public static void sort(Object[] a, Comparator c) {
+        sort(a, c, SortOrder.ASC);
+    }
+
+    /**
+     * Rearranges the array in specific order, using a comparator.
+     *
+     * @param a the array
+     * @param c the comparator specifying the order
+     */
+    public static void sort(Object[] a, Comparator c, SortOrder order) {
+        if (order == SortOrder.ASC)
+            sortASC(a, c);
+        else
+            sortDESC(a, c);
+    }
+
+    /**
      * Rearranges the array in ascending order, using the natural order.
      *
      * @param a the array to be sorted
@@ -54,6 +79,23 @@ public class SelectionSort {
     }
 
     /**
+     * Rearranges the array in ascending order, using a comparator.
+     *
+     * @param a the array
+     * @param c the comparator specifying the order
+     */
+    protected static void sortASC(Object[] a, Comparator c) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(c, a[j], a[min]))
+                    min = j;
+            }
+        }
+    }
+
+    /**
      * Rearranges the array in descending order, using the natural order.
      *
      * @param a the array to be sorted
@@ -67,6 +109,23 @@ public class SelectionSort {
                     max = j;
             }
             exch(a, i, max);
+        }
+    }
+
+    /**
+     * Rearranges the array in descending order, using a comparator.
+     *
+     * @param a the array
+     * @param c the comparator specifying the order
+     */
+    protected static void sortDESC(Object[] a, Comparator c) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (greater(c, a[j], a[min]))
+                    min = j;
+            }
         }
     }
 }
