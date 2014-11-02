69c5e5257b663aa9cbfdb46ce0d29392c8a05b7a
BigEgg <cd_bigegg@hotmail.com>
2014-11-02 19:52:44 +0800

    Add the unit tests for Quick Sort

---
+++ c#/BigEgg.Algorithm/BigEgg.Algorithm.Test/Sorts/QuickSortTest.cs
@@ -0,0 +1,169 @@
+﻿using BigEgg.Algorithm.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BigEgg.Algorithm.Test.Sorts
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTest_NullArray()
+        {
+            QuickSort.Sort(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTest_ASCOrder_NullArray()
+        {
+            QuickSort.Sort(null, SortOrder.ASC);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTest_DESCOrder_NullArray()
+        {
+            QuickSort.Sort(null, SortOrder.DESC);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTestWithComparer_NullArray()
+        {
+            QuickSort.Sort(null, Point.X_ORDER);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTestWithComparer_NullComparer()
+        {
+            Point[] points = new Point[10];
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(i, 10 - i);
+            }
+
+            QuickSort.Sort(points, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTestWithComparer_ASCOrder_NullArray()
+        {
+            QuickSort.Sort(null, Point.X_ORDER, SortOrder.ASC);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTestWithComparer_ASCOrder_NullComparer()
+        {
+            Point[] points = new Point[10];
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(i, 10 - i);
+            }
+
+            QuickSort.Sort(points, null, SortOrder.ASC);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTestWithComparer_DESCOrder_NullArray()
+        {
+            QuickSort.Sort(null, Point.X_ORDER, SortOrder.DESC);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTestWithComparer_DESCOrder_NullComparer()
+        {
+            Point[] points = new Point[10];
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(i, 10 - i);
+            }
+
+            QuickSort.Sort(points, null, SortOrder.DESC);
+        }
+
+        [TestMethod]
+        public void SortTest()
+        {
+            String[] a = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Shuffle.Do(a);
+
+            Assert.IsFalse(SortHelper.IsSorted(a));
+            QuickSort.Sort(a);
+            Assert.IsTrue(SortHelper.IsSorted(a));
+        }
+
+        [TestMethod]
+        public void SortTest_ASC()
+        {
+            String[] a = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Shuffle.Do(a);
+
+            Assert.IsFalse(SortHelper.IsSorted(a, SortOrder.ASC));
+            QuickSort.Sort(a, SortOrder.ASC);
+            Assert.IsTrue(SortHelper.IsSorted(a, SortOrder.ASC));
+        }
+
+        [TestMethod]
+        public void SortTest_DESC()
+        {
+            String[] a = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Shuffle.Do(a);
+
+            Assert.IsFalse(SortHelper.IsSorted(a, SortOrder.DESC));
+            QuickSort.Sort(a, SortOrder.DESC);
+            Assert.IsTrue(SortHelper.IsSorted(a, SortOrder.DESC));
+        }
+
+        [TestMethod]
+        public void SortTestWithComparer()
+        {
+            Point[] points = new Point[10];
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(i, 10 - i);
+            }
+            Shuffle.Do(points);
+
+            Assert.IsFalse(SortHelper.IsSorted(points, Point.X_ORDER));
+            QuickSort.Sort(points, Point.X_ORDER);
+            Assert.IsTrue(SortHelper.IsSorted(points, Point.X_ORDER));
+        }
+
+        [TestMethod]
+        public void SortTestWithComparer_ASC()
+        {
+            Point[] points = new Point[10];
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(i, 10 - i);
+            }
+            Shuffle.Do(points);
+
+            Assert.IsFalse(SortHelper.IsSorted(points, Point.X_ORDER, SortOrder.ASC));
+            QuickSort.Sort(points, Point.X_ORDER, SortOrder.ASC);
+            Assert.IsTrue(SortHelper.IsSorted(points, Point.X_ORDER, SortOrder.ASC));
+        }
+
+        [TestMethod]
+        public void SortTestWithComparer_DESC()
+        {
+            Point[] points = new Point[10];
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(i, 10 - i);
+            }
+            Shuffle.Do(points);
+
+            Assert.IsFalse(SortHelper.IsSorted(points, Point.X_ORDER, SortOrder.DESC));
+            QuickSort.Sort(points, Point.X_ORDER, SortOrder.DESC);
+            Assert.IsTrue(SortHelper.IsSorted(points, Point.X_ORDER, SortOrder.DESC));
+        }
+    }
+}
