4e3396239e8a781debf1cc94a67e1e47a94b5b95
BigEgg <cd_bigegg@hotmail.com>
2014-11-08 16:11:12 +0800

    Add the interface for the priority queue

---
+++ java/Algorithms/src/Collections/IPriorityQueue.java
@@ -0,0 +1,43 @@
+package Collections;
+
+/**
+ * Created by jianming.xiao on 11/8/14.
+ */
+public interface IPriorityQueue<Key> extends Iterable<Key> {
+    /**
+     * Is the priority queue empty?
+     *
+     * @return true if the priority queue is empty; false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns the number of keys on the priority queue.
+     *
+     * @return the number of keys on the priority queue
+     */
+    int size();
+
+    /**
+     * Returns a first key on the priority queue.
+     *
+     * @return a first key on the priority queue
+     * @throws java.util.NoSuchElementException if the priority queue is empty
+     */
+    Key first();
+
+    /**
+     * Adds a new key to the priority queue.
+     *
+     * @param x the new key to add to the priority queue
+     */
+    void insert(Key x);
+
+    /**
+     * Removes and returns a first key on the priority queue.
+     *
+     * @return the first key on the priority queue
+     * @throws java.util.NoSuchElementException if priority queue is empty.
+     */
+    Key delete();
+}
