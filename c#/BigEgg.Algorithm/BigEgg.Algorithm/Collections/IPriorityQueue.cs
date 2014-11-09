dd8f49cb185e9cdac46e825988abd4060f90c9c2
BigEgg <cd_bigegg@hotmail.com>
2014-11-09 18:42:44 +0800

    Add the abstract class for the priority queue.

--- c#/BigEgg.Algorithm/BigEgg.Algorithm/Collections/IPriorityQueue.cs
+++ c#/BigEgg.Algorithm/BigEgg.Algorithm/Collections/IPriorityQueue.cs
@@ -8,30 +8,33 @@ namespace BigEgg.Algorithm.Collections
         /// Is the priority queue empty?
         /// </summary>
         /// <returns><c>True</c> if the priority queue is empty; <c>false</c> otherwise.</returns>
-        bool isEmpty();
+        bool IsEmpty();
 
         /// <summary>
         /// Returns the number of keys on the priority queue.
         /// </summary>
         /// <returns>The number of keys on the priority queue.</returns>
-        int size();
+        int Size();
 
         /// <summary>
         /// Returns the first key on the priority queue.
         /// </summary>
         /// <returns>The first key on the priority queue.</returns>
-        Key first();
+        /// <exception cref="System.ArgumentOutOfRangeException">Queue is empty.</exception>
+        Key First();
 
         /// <summary>
         /// Adds a new key to the priority queue.
         /// </summary>
         /// <param name="x">The new key to add to the priority queue.</param>
-        void insert(Key x);
+        /// <exception cref="System.ArgumentNullException">Item cannot be null.</exception>
+        void Insert(Key x);
 
         /// <summary>
         /// Removes and returns a first key on the priority queue.
         /// </summary>
         /// <returns>The first key on the priority queue.</returns>
-        Key delete();
+        /// <exception cref="System.ArgumentOutOfRangeException">Queue is empty.</exception>
+        Key Delete();
     }
 }
